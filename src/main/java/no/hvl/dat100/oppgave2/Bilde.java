270e82705ac562ba025ae408c247144b1d3aa9d2
gronningencode <vebjorn.gronningen@gmail.com>
2025-11-12 19:38:10 +0100

    =)
    oppgave 2 og 4

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -1,33 +1,32 @@
 package no.hvl.dat100.oppgave2;
 
-import no.hvl.dat100.common.TODO;
-
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
-	
-	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
-	}
+    private String url;
 
-	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
-	}
-	
-	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+    public Bilde(int id, String bruker, String dato, String tekst, String url) {
+        super(id, bruker, dato, tekst);
+        this.url = url;
+    }
 
-	}
+    public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
+        super(id, bruker, dato, likes, tekst);
+        this.url = url;
+    }
 
-	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	}
+    @Override
+    public String toString() {
+        // Format: "BILDE\n" + (tekst fra super) + url + "\n"
+        return "BILDE\n" + super.toString() + url + "\n";
+    }
 
 	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -3,13 +3,27 @@ package no.hvl.dat100.oppgave4;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.common.TODO;
-import no.hvl.dat100.oppgave3.*;
+import no.hvl.dat100.oppgave3.Blogg;
+import no.hvl.dat100.oppgave1.Innlegg;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+        String filsti = mappe + "/" + filnavn;
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+        try (PrintWriter writer = new PrintWriter(filsti)) {
+            writer.println(samling.getAntall());
+
+            for (int i = 0; i < samling.getAntall(); i++) {
+                Innlegg innlegg = samling.getInnlegg(i);
+                writer.print(innlegg.toString());
+            }
+
+            return true;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
